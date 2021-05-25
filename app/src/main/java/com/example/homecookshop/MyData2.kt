ab59ad8bb019dc0a43f36e85b942c4eda9a9acf8
DESKTOP-5I3O0RN\LG-11 <cjrcjr002@naver.com>
2021-05-25 21:41:43 +0900

    no message

--- app/src/main/java/com/example/homecookshop/MyData2.kt
+++ app/src/main/java/com/example/homecookshop/MyData2.kt
@@ -2,5 +2,5 @@ package com.example.homecookshop
 
 import java.io.Serializable
 
-data class MyData2(var word:String, var meaning:String, var food:String, var isOpen:Boolean):Serializable {
+data class MyData2(var menu_name:String, var word:String, var meaning:String, var food:String, var isOpen:Boolean):Serializable {
 }
