60908e9a437e6f7d2b7a33b03378a179d91d2ba6
DESKTOP-5I3O0RN\LG-11 <cjrcjr002@naver.com>
2021-06-08 17:54:18 +0900

    ref 추가 반절 완성

---
+++ app/src/main/java/com/example/homecookshop/RefActivity2.kt
@@ -0,0 +1,64 @@
+package com.example.homecookshop
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_ref.*
+import kotlinx.android.synthetic.main.activity_ref.addRef
+import kotlinx.android.synthetic.main.activity_ref2.*
+
+class RefActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ref2)
+
+        addRef2.setOnClickListener {
+            material_modal("", "", "", "", 0)
+        }
+    }
+
+    private fun material_modal(str_name:String, str_word:String, str_meaning:String, str_food: String, position:Int){
+        var builder  = AlertDialog.Builder(this)
+        builder.setTitle("현재 보유중인 재료 추가")
+        builder.setView(R.mipmap.ic_launcher)
+
+        var v1 = layoutInflater.inflate(R.layout.material_modal,null)
+        builder.setView(v1)
+        val material_Dialog = builder.show()
+
+        val confirm_Button = v1.findViewById<Button>(R.id.material_confirm)
+        val cancle_button = v1.findViewById<Button>(R.id.material_cancle)
+        val delete_button = v1.findViewById<Button>(R.id.material_delete)
+
+        val edit_material = v1.findViewById<EditText>(R.id.edit_material)
+        val edit_unit = v1.findViewById<EditText>(R.id.edit_unit)
+        val edit_count = v1.findViewById<EditText>(R.id.edit_count)
+
+
+        edit_material.setText(str_word)
+        edit_count.setText(str_meaning)
+        edit_unit.setText(str_food)
+
+        confirm_Button.setOnClickListener {
+            material_Dialog.dismiss()
+
+/*
+            val intent = Intent(this, RefActivity3::class.java)
+*/
+            startActivity(intent)
+
+        }
+        cancle_button.setOnClickListener {
+            material_Dialog.dismiss()
+        }
+        delete_button.setOnClickListener {
+            Log.v("as", position.toString())
+            material_Dialog.dismiss()
+            //해당 재료 삭제
+        }
+    }
+}
